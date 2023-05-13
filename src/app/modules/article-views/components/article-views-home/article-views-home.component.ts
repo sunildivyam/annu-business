600ec9189c4dc9ed089264d29ec8ff99bb07369b
Sunil Kumar <sunil.divyam@gmail.com>
2023-05-13 16:39:34 +0530

    Home Page - page metainfo is not getting updated when coming back from any other page on client side
    Closes #31

--- src/app/modules/article-views/components/article-views-home/article-views-home.component.ts
+++ src/app/modules/article-views/components/article-views-home/article-views-home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Category, PageCategoryGroup, MetaService } from '@annu/ng-lib';
 
 import { environment } from '../../../../../environments/environment';
 import { ARTICLE_VIEWS_ROUTE_RESOLVER_DATA_KEYS } from '../../constants/article-views.constants';
 import { ArticlesHomeViewRouteData } from '../../interfaces/article-views.interface';
+import { Subscription, filter } from 'rxjs';
 const { appConfig } = environment;
 
 const DEFAULT_DESCRIPTION_CHAR_COUNT = 300;
@@ -21,6 +22,7 @@ export class ArticleViewsHomeComponent implements OnInit, OnDestroy {
   error: any;
 
   appConfig = { ...appConfig };
+  navigationEndSubscription: Subscription;
 
   // if a category or article does not exist, then these variables will hold the ids of the same and will get thme from queryparams.
   notFoundCategoryId: string = '';
@@ -32,14 +34,18 @@ export class ArticleViewsHomeComponent implements OnInit, OnDestroy {
       // Sets not found category and/or article ids, in case user is redirected here from respective pages.
       this.notFoundCategoryId = params['categoryId'] || '';
       this.notFoundArticleId = params['articleId'] || '';
-    })
+    });
+
+    this.navigationEndSubscription = this.router.events.pipe(filter(ev => ev instanceof NavigationEnd)).subscribe(() => this.setPageMeta());
   }
 
   ngOnInit(): void {
     this.initFromResolvedData(this.route.snapshot.data);
   }
 
-  ngOnDestroy(): void { }
+  ngOnDestroy(): void {
+    this.navigationEndSubscription.unsubscribe();
+  }
 
   private initFromResolvedData(data: any): void {
     const homeViewData = { ...data[ARTICLE_VIEWS_ROUTE_RESOLVER_DATA_KEYS.ARTICLES_HOME_VIEW] ?? null } as ArticlesHomeViewRouteData;
@@ -50,6 +56,10 @@ export class ArticleViewsHomeComponent implements OnInit, OnDestroy {
       .filter((cg: PageCategoryGroup) => cg.category?.isFeatured === true)
       .map((cg: PageCategoryGroup) => ({ ...cg.category as Category })) || [];
 
+    this.setPageMeta();
+  }
+
+  public setPageMeta(): void {
     if (!this.route.firstChild) {
       this.metaService.setPageMeta(appConfig.metaInfo);
     }
